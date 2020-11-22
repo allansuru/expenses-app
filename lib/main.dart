6d9d05654ce59f671c7ff9deb1e50f37f8a4948f
SuRu <allansuru@hotmail.com>
2020-11-22 15:46:14 -0300

    estilzando componentes no flutter - valor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyHomePage extends StatelessWidget {
   final _transactions = [
     Transaction(
         id: 't1',
-        title: 'Novo Tenis de corrida',
+        title: 'Novo Tênis de corrida',
         value: 310.86,
         date: DateTime.now()),
     Transaction(
@@ -33,11 +33,37 @@ class MyHomePage extends StatelessWidget {
           Container(
             child: Card(
               child: Text('Gráfico'),
-              elevation: 5,
               color: Colors.blue,
             ),
           ),
-          Card(child: Text('Lista de transações'))
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple[100],
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    children: [Text(tr.title), Text(tr.date.toString())],
+                  )
+                ]),
+                elevation: 4,
+              );
+            }).toList(),
+          )
         ],
       ),
     );
